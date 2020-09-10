2de4d41406c9e30354b685193607c82ca588c475
erikc <erikciqueira.silva@gmail.com>
2020-09-10 16:09:05 -0300

    app tasks

---
+++ app/src/main/java/com/example/tasks/service/listener/APIListener.kt
@@ -0,0 +1,9 @@
+package com.example.tasks.service.listener
+
+import com.example.tasks.service.model.TaskModel
+
+interface APIListener<T> {
+    fun onSuccess(model: T)
+    fun onFailure(str: String)
+
+}
